29dcbc8b841d0703a8903c5042d4ffa2bff49970
P.E. Butler III <p.e.butler3@gmail.com>
2015-03-19 08:32:08 -0600

    updated content / a real comment isn' necessary

--- harmony/js/app.js
+++ harmony/js/app.js
@@ -1,1 +1,9 @@
 $(document).foundation();
+$(function() {
+  $('nav li').each(function() {
+    var href = $(this).find('a').attr('href');
+    if (href === window.location.pathname) {
+      $(this).addClass('active');
+    }
+  });
+});
