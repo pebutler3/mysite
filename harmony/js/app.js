7dba8577eedda946aac30ec4547ebda0bf429945
P.E. Butler III <p.e.butler3@gmail.com>
2015-06-22 23:02:33 -0600

    removed as well old and unneeded

--- harmony/js/app.js
+++
@@ -1,9 +0,0 @@
-$(document).foundation();
-$(function() {
-  $('nav li').each(function() {
-    var href = $(this).find('a').attr('href');
-    if (href === window.location.pathname) {
-      $(this).addClass('active');
-    }
-  });
-});
